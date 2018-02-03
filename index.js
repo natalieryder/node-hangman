8dc7373279a4df46d8e4d9da68b6e8bcd230149d
Natalie Ryder <nryder121@pointloma.edu>
2018-02-03 01:12:59 -0800

    change api search function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ inquirer.prompt([{
 })
 
 function createGame(topic) {
-	request('https://api.datamuse.com/words?topics=' + topic + '&max=10', function (error, response, body) {
+	request('https://api.datamuse.com/words?rel_trg=' + topic + '&max=10', function (error, response, body) {
 		if (error) {
 	  		console.log('error:', error); // Print the error if one occurred
 	  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
